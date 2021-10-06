705b4395fc6c9744335491f2002832ccd08dc7ad
gurjitsingh <Gurjit_47>
2021-10-06 15:55:54 +1100

    Cognito user pool

---
+++ src/UserPool.js
@@ -0,0 +1,8 @@
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+
+const poolData = {
+    UserPoolId: "ap-southeast-2_9MxkijGq6",
+    ClientId: "6a6qnam5gbutmvj4j8gt1jcu07"
+}
+
+export default new CognitoUserPool(poolData);
